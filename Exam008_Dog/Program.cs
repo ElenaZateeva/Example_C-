9c16490c7c75bbdf8b5558994b2fbf1dbc31d2cc
ElenaZateeva <zateevaelena14@gmail.com>
2022-10-18 14:04:27 +0300

    Два друга и собака

---
+++ Exam008_Dog/Program.cs
@@ -0,0 +1,22 @@
+﻿int distance = 1000, firstFriendSpeed = 1,
+    dogSpeed = 5, secondFriendSpeed = 2,
+    friend = 2, count = 0;
+ int timeA = 0;
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        timeA = distance / (firstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        timeA = distance / (secondFriendSpeed + dogSpeed);
+        friend = 1; 
+    }
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * timeA;
+    count++;
+}
+Console.Write("Собака пробежит ", count, " раз.");
+Console.Write(count);
+Console.WriteLine(" раз.");
